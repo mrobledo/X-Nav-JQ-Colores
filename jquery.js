69354c1a8f36204d2acb564ed07598899a27d158
mariano <mariano@mariano-MacBookAir.(none)>
2015-02-23 13:25:51 +0100

    v1

---
+++ jquery.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$("#orderedlist").addClass("red");
+	$("#orderedlist li").addClass("blue");
+	$("#orderedlist li:last").hover(function() {
+		$(this).addClass("green");
+	},
+	function(){
+		$(this).removeClass("green");
+	});
+});
